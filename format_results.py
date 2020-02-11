83e6c0637d70fbd86afee8da159f08d1514f2463
Brian Hopkins <b.hopkins@rasa.com>
2020-02-11 06:49:14 -0500

    adding in debug to look into PR comment issue

--- format_results.py
+++ format_results.py
@@ -17,6 +17,7 @@ def format_cell(data, c, k):
     if not data[c].get(k):
         return "N/A"
     if k == "confused_with":
+        print(f"The confused with line is: {k}")
         return ", ".join([f"{k}({v})" for k,v in data[c][k].items()])
     else:
         return data[c][k]
