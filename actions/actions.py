0b930a21e186089a8b8bbdca124b3c38bb5e4731
melindaloubser1 <melinda.loubser@gmail.com>
2020-05-21 14:18:20 -0400

    retrieve incident status

--- actions/actions.py
+++ actions/actions.py
@@ -1,90 +1,92 @@
 import logging
-from typing import Dict, Text, Any, List, Union
+from typing import Dict, Text, Any, List, Union, Optional
 from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
-from rasa_sdk.events import AllSlotsReset
-import ruamel.yaml
-
-import requests
-import json
-import pathlib
+from rasa_sdk.forms import FormAction, REQUESTED_SLOT
+from rasa_sdk.events import AllSlotsReset, SlotSet, EventType
+from actions.snow import SnowAPI
+import random
 
 logger = logging.getLogger(__name__)
 vers = "vers: 0.1.0, date: Apr 2, 2020"
 logger.debug(vers)
 
-here = pathlib.Path(__file__).parent.absolute()
-snow_config = (
-    ruamel.yaml.safe_load(open(f"{here}/snow_credentials.yml", "r")) or {}
-)
-snow_user = snow_config.get("snow_user")
-snow_pw = snow_config.get("snow_pw")
-snow_instance = snow_config.get("snow_instance")
-localmode = snow_config.get("localmode", True)
-logger.debug(f"Local mode: {localmode}")
-
-base_api_url = "https://{}/api/now".format(snow_instance)
+snow = SnowAPI()
+localmode = snow.localmode
+logger.debug(f"Local mode: {snow.localmode}")
 
 
-def email_to_sysid(email):
-    lookup_url = f"{base_api_url}/table/sys_user?sysparm_limit=1&email={email}"
-    user = snow_user
-    pwd = snow_pw
-    # Set proper headers
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }  # noqa: 501
-    results = dict()
-    results["status"] = 200
-    # Do the HTTP request
-    try:
-        response = requests.get(lookup_url, auth=(user, pwd), headers=headers)
-        if response.status_code == 200:
-            results["value"] = response.json()["result"]
-        else:
-            results["status"] = response.status_code
-            results["msg"] = (
-                "ServiceNow error: " + response.json()["error"]["message"]
-            )
-    except requests.exceptions.Timeout:
-        results["msg"] = "Could not connect to ServiceNow (Timeout)"
-    return results
 
 
-def create_incident(description, short_description, priority, caller):
-    incident_url = "https://{}/api/now/table/incident".format(snow_instance)
-    user = snow_user
-    pwd = snow_pw
-    # Set proper headers
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    }  # noqa: 501
-    data = {
-        "opened_by": caller,
-        "short_description": short_description,
-        "description": description,
-        "urgency": priority,
-        "caller_id": caller,
-        "comments": description,
-    }
-    response = requests.post(
-        incident_url, auth=(user, pwd), headers=headers, data=json.dumps(data)
-    )
-    return response
+def custom_request_next_slot(
+    form,
+    dispatcher: "CollectingDispatcher",
+    tracker: "Tracker",
+    domain: Dict[Text, Any],
+) -> Optional[List[EventType]]:
+    """Request the next slot and utter template if needed,
+        else return None"""
 
+    for slot in form.required_slots(tracker):
+        if form._should_request_slot(tracker, slot):
+            logger.debug(f"Request next slot '{slot}'")
+            if slot == "email" and tracker.get_slot("previous_email"):
+                dispatcher.utter_message(
+                    template=f"utter_ask_use_previous_email", **tracker.slots
+                    )
+            else:
+                dispatcher.utter_message(
+                    template=f"utter_ask_{slot}", **tracker.slots
+                )
+            return [SlotSet(REQUESTED_SLOT, slot)]
+
+    return None
+
+def _validate_email(
+    value: Text,
+    dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: Dict[Text, Any],
+) -> Dict[Text, Any]:
+    """Validate email is in ticket system."""
+    if not value:
+        return {"email": None, "previous_email": None}
+    elif isinstance(value, bool):
+        value = tracker.get_slot("previous_email")
+
+    if localmode:
+        return {"email": value}
+
+    results = snow.email_to_sysid(value)
+    caller_id = results.get("caller_id")
+
+    if caller_id:
+        return {"email": value, "caller_id": caller_id}
+    elif isinstance(caller_id, list):
+        dispatcher.utter_message(template="utter_no_email")
+        return {"email": None}
+    else:
+        dispatcher.utter_message(results.get("error"))
+        return {"email": None}
 
 class OpenIncidentForm(FormAction):
     def name(self) -> Text:
         return "open_incident_form"
 
+    def request_next_slot(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any],
+    ) -> Optional[List[EventType]]:
+
+        return custom_request_next_slot(self, dispatcher, tracker, domain)
+
     @staticmethod
     def required_slots(tracker: Tracker) -> List[Text]:
         """A list of required slots that the form has to fill"""
 
-        return ["email", "priority", "problem_description", "incident_title"]
+        return ["email", "priority", "problem_description", "incident_title", "confirm"]
 
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
         """A dictionary to map required slots to
@@ -94,11 +96,15 @@ class OpenIncidentForm(FormAction):
             or a list of them, where a first match will be picked"""
 
         return {
-            "email": self.from_entity(entity="email"),
+            "email": [
+                self.from_entity(entity="email"),
+                self.from_intent(value=True, intent="affirm"),
+                self.from_intent(value=False, intent="deny"),
+            ],
             "priority": self.from_entity(entity="priority"),
             "problem_description": [
                 self.from_text(
-                    intent=["password_reset", "problem_email", "inform"]
+                    not_intent = ["incident_status", "bot_challenge", "help", "out_of_scope", "affirm", "deny"]
                 )
             ],
             "incident_title": [
@@ -109,16 +115,14 @@ class OpenIncidentForm(FormAction):
                     intent="problem_email", value="Problem with email"
                 ),
                 self.from_text(
-                    intent=["password_reset", "problem_email", "inform"]
+                    not_intent = ["incident_status", "bot_challenge", "help", "out_of_scope", "affirm", "deny"]
                 ),
             ],
+            "confirm": [
+                self.from_intent(value=True, intent="affirm"),
+                self.from_intent(value=False, intent="deny"),
+            ],
         }
-
-    @staticmethod
-    def priority_db() -> List[Text]:
-        """Database of supported priorities"""
-
-        return ["low", "medium", "high"]
 
     def validate_email(
         self,
@@ -128,22 +132,7 @@ class OpenIncidentForm(FormAction):
         domain: Dict[Text, Any],
     ) -> Dict[Text, Any]:
         """Validate email is in ticket system."""
-        if localmode:
-            return {"email": value}
-        results = email_to_sysid(value)
-
-        if results["status"] == 200:
-            # validation succeeded, set the value of the "email" slot to value
-            if len(results["value"]) == 1:
-                return {"email": value}
-            else:
-                dispatcher.utter_message(template="utter_no_email")
-                return {"email": None}
-        else:
-            dispatcher.utter_message(results["msg"])
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
-            return {"email": None}
+        return _validate_email(value, dispatcher, tracker, domain)
 
     def validate_priority(
         self,
@@ -154,14 +143,10 @@ class OpenIncidentForm(FormAction):
     ) -> Dict[Text, Any]:
         """Validate priority is a valid value."""
 
-        if value.lower() in self.priority_db():
-            # validation succeeded,
-            # set the value of the "priority" slot to value
+        if value.lower() in snow.priority_db():
             return {"priority": value}
         else:
             dispatcher.utter_message(template="utter_no_priority")
-            # validation failed, set this slot to None, meaning the
-            # user will be asked for the slot again
             return {"priority": None}
 
     def submit(
@@ -170,21 +155,19 @@ class OpenIncidentForm(FormAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict]:
-        """Define what the form has to do
-            after all required slots are filled"""
+        """Create an incident and return details or
+        if localmode return incident details as if incident
+        was created
+        """
 
         priority = tracker.get_slot("priority")
         email = tracker.get_slot("email")
         problem_description = tracker.get_slot("problem_description")
         incident_title = tracker.get_slot("incident_title")
-
-        # Check priority and set number value accordingly
-        if priority == "low":
-            snow_priority = "3"
-        elif priority == "medium":
-            snow_priority = "2"
-        else:
-            snow_priority = "1"
+        confirm = tracker.get_slot("confirm")
+        if not confirm:
+            dispatcher.utter_message(template="utter_incident_creation_canceled")
+            return [AllSlotsReset(), SlotSet("previous_email", email)]
 
         if localmode:
             message = (
@@ -195,19 +178,110 @@ class OpenIncidentForm(FormAction):
                 f"title: {incident_title}\npriority: {priority}"
             )
         else:
-            results = email_to_sysid(email)
-            sysid = results["value"][0]["sys_id"]
-            response = create_incident(
+            snow_priority = snow.priority_db().get(priority)
+            response = snow.create_incident(
                 description=problem_description,
                 short_description=incident_title,
                 priority=snow_priority,
-                caller=sysid,
+                email=email,
             )
-            incident_number = response.json()["result"]["number"]
-            message = (
-                f"Successfully opened up incident {incident_number} for you.  "
-                f"Someone will reach out soon."
-            )
-            # utter submit template
+            incident_number = response.get("content", {}).get("result", {}).get("number")
+            if incident_number:
+                message = (
+                    f"Successfully opened up incident {incident_number} for you.  "
+                    f"Someone will reach out soon."
+                )
+            else:
+                message = (
+                    f"Something went wrong while opening an incident for you. "
+                    f"{response.get('error')}"
+                )
         dispatcher.utter_message(message)
-        return [AllSlotsReset()]
+        return [AllSlotsReset(), SlotSet("previous_email", email)]
+
+
+class IncidentStatusForm(FormAction):
+    def name(self) -> Text:
+        return "incident_status_form"
+
+    def request_next_slot(
+        self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any],
+    ) -> Optional[List[EventType]]:
+
+        return custom_request_next_slot(self, dispatcher, tracker, domain)
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["email"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "email": [
+                self.from_entity(entity="email"),
+                self.from_intent(value=True, intent="affirm"),
+                self.from_intent(value=False, intent="deny"),
+            ]
+        }
+
+    def validate_email(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate email is in ticket system."""
+        return _validate_email(value, dispatcher, tracker, domain)
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Look up all incidents associated with email address
+           and return status of each"""
+
+        email = tracker.get_slot("email")
+
+        incident_states = {
+            "New": "is currently awaiting triage",
+            "In Progress": "is currently in progress",
+            "On Hold": "has been put on hold",
+            "Closed": "has been closed"
+        }
+        if localmode:
+            status = random.choice(incident_states.values())
+            message = (
+                f"Since ServiceNow isn't connected, I'm making this up!"
+                f"The most recent incident for {email} {status}"
+            )
+        else:
+            incidents_result = snow.retrieve_incidents(email)
+            incidents = incidents_result.get("incidents")
+            if incidents:
+                message = "\n".join([
+                    f'Incident {incident.get("number")}: "{incident.get("short_description")}", '
+                    f'opened on {incident.get("opened_at")} {incident_states.get(incident.get("incident_state"))}'
+                    for incident in incidents
+                ])
+
+            else:
+                message = (
+                    f"{incidents_result.get('error')}"
+                )
+
+        dispatcher.utter_message(message)
+        return [AllSlotsReset(), SlotSet("previous_email", email)]
+
